52266cb741051916703082fcd8292a16648a496b
michla <michla350@outlook.com>
2023-05-24 18:13:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizationService } from '../AuthorizationService;
+import { Authorization } from '../hero.interface';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
-export class AppComponent {
-  title = 'authClient';
+export class DashboardComponent implements OnInit {
+  users: Authorization[] = [];
+
+  constructor(private authService: AuthorizationService) { }
+
+  ngOnInit(): void {
+    this.users = authService.getUsers();
+  }
+
+  getUsers(): void {
+    this.authService.getUsers()
+      .subscribe(usersByName => this.users = usersByName.slice(1, 5));
+  }
 }
